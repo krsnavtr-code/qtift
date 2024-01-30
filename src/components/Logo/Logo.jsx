9ee63a4c3badc7c504518a146328f1dfbe3a1b4b
Krishna Avtar <krsnavtr@gmail.com>
2024-01-30 20:25:32 +0530

    create Navbar.module.css file and style in it

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./Logo.module.css";
 import logoImage from "../../assets/logoimage.png";
 
 const Logo = () => {
   return (
-    <>
+    <div className={styles.logoWrapper}>
       <img src={logoImage} alt="logo" width={67} />
-    </>
+    </div>
   );
 };
 
